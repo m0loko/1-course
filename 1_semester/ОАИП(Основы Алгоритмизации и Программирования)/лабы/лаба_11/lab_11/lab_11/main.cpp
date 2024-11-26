e06d354319491d643ed6d08528b0700e7f995bb0
m0loko <gittigwork@gmail.com>
2024-11-26 22:57:24 +0300

    5th commit without good folder structuring

--- 1_semester/ОАИП(Основы Алгоритмизации и Программирования)/лабы/лаба_11/lab_11/lab_11/main.cpp
+++ 1_semester/ОАИП(Основы Алгоритмизации и Программирования)/лабы/лаба_11/lab_11/lab_11/main.cpp
@@ -1,32 +1,45 @@
 #include <iostream>
-#include <bitset>
-
 using namespace std;
 
 int main() {
-    setlocale(LC_CTYPE, "ru");
-
-    // Ввод чисел и параметров
-    char str[33];
-    int A, B, n, p, m, q,c=0;
-    cout << "Введите число A: ";
+    setlocale(LC_ALL, "Russian");
+    int A, B, n, p, q,m;
+    char tmp[33];
+    cout << "Введите целое число A: ";
     cin >> A;
-    _itoa_s(A, str, 2);
-    cout << str << endl;
-    cout << "Введите число B: ";
+    cout << "Введите целое число B: ";
     cin >> B;
-    cout << "Введите количество битов n, которые необходимо установить в 0 в A: ";
+    cout << "Сколько битов числа A нужно установить в 0? ";
     cin >> n;
-    cout << "Введите позицию p, от которой необходимо начать устанавливать биты в 0 в A: ";
+    cout << "Введите позицию p в числе A, вправо от которой нужно установить в 0 биты: ";
     cin >> p;
-    //cout << "Введите количество битов m, которые необходимо заменить в B: ";
-    //cin >> m;
-    //cout << "Введите позицию q, от которой необходимо начать замену битов в B: ";
-    //cin >> q;
-    int maskA = ((1 << n) - 1)<<(p-1);
-    _itoa_s(maskA, str, 2);
-    cout << str<<endl;
-    _itoa_s(((maskA & A) | c)>>(p-1), str, 2);
-    cout << str;
+    cout << "Сколько битов числа B нужно заменить? ";
+    cin >> m;
+    cout << "Введите позицию q в числе B, начиная с которой нужно заменить биты: ";
+    cin >> q;
+    _itoa_s(A, tmp, 2);
+    cout << "Число A в двоичной системе: " << tmp << endl;
+    _itoa_s(B, tmp, 2);
+    cout << "Число B в двоичной системе: " << tmp << endl;
+    // Создание маски для установки n битов в 0 влево от позиции p
+    int maskA = ((1 << n) - 1) << (p - n);
+    _itoa_s(maskA, tmp, 2);
+    cout << "Маска A: " << tmp << endl;
+    // Установка n битов в 0 в числе A
+    A = A & ~maskA;
+    _itoa_s(A, tmp, 2);
+    cout << "Число A после установки битов в 0: " << tmp << endl;
+    // Извлечение n битов из числа A
+    int extractedBits = (A & maskA) >> p;
+    _itoa_s(extractedBits, tmp, 2);
+    cout << "Извлеченные биты из числа A: " << tmp << endl;
+    // Создание маски для замены m битов в числе B начиная с позиции q
+    int maskB = ((1 << m) - 1) << q;
+    _itoa_s(maskB, tmp, 2);
+    cout << "Маска B: " << tmp << endl;
+    // Очистка m битов в числе B и замена их извлеченными битами
+    B = (B & ~maskB) | (extractedBits << (q - 1));
+    _itoa_s(B, tmp, 2);
+    cout << "Число B после замены битов: " << tmp << endl;
     return 0;
 }
--- 1_semester/ОАИП(Основы Алгоритмизации и Программирования)/лабы/лаба_12/lab_12/lab_12/lab_12.cpp
+++ 1_semester/ОАИП(Основы Алгоритмизации и Программирования)/лабы/лаба_12/lab_12/lab_12/lab_12.cpp
@@ -1,16 +1,81 @@
 #include <iostream>
+#include <Windows.h>
+#include <ctime>
+using namespace std;
 
-int main()
-{
-    double d{ 10.6 };
-    double* pd{ &d };
-    std::cout << "Pointer pd: address:" << pd << std::endl;
-    pd++;   // увеличение адреса на 8 байт - размер double
-    std::cout << "Pointer pd: address:" << pd << std::endl;
+void peres(int* X, int* Y, int k, int n) {
+    int minSize = (k < n) ? k : n;
+    int* Z = new int[minSize];
+    int zSize = 0;
 
-    short n{ 5 };
-    short* pn{ &n };
-    std::cout << "Pointer pn: address:" << pn << std::endl;
-    pn++;   // увеличение адреса на 2 байта - размер short
-    std::cout << "Pointer pn: address:" << pn << std::endl;
+    for (int i = 0; i < k; i++) {
+        for (int j = 0; j < n; j++) {
+            if (*(X + i) == *(Y + j)) {
+                bool exists = false;
+                for (int l = 0; l < zSize; l++) {
+                    if (*(Z + l) == *(X + i)) {
+                        exists = true;
+                        break;
+                    }
+                }
+                if (!exists) {
+                    *(Z + zSize) = *(X + i);
+                    zSize++;
+                }
+            }
+        }
+    }
+
+    cout << "\nМассив пересечения Z: ";
+    if (zSize == 0) {
+        cout << "Нет общих элементов.";
+    }
+    else {
+        for (int i = 0; i < zSize; i++) {
+            cout << *(Z + i) << " ";
+        }
+    }
+    cout << endl;
+
+    delete[] Z;
+}
+
+int main() {
+    srand(static_cast<unsigned>(time(0)));
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int k, n;
+    cout << "Введите размер массива X: ";
+    cin >> k;
+    cout << "Введите размер массива Y: ";
+    cin >> n;
+    int* X = new int[k];
+    int* Y = new int[n];
+
+    for (int i = 0; i < k; i++) {
+        *(X + i) = rand() % 101;
+    }
+    for (int i = 0; i < n; i++) {
+        *(Y + i) = rand() % 101;
+    }
+
+    cout << "Массив X: ";
+    for (int i = 0; i < k; i++) {
+        cout << *(X + i) << " ";
+    }
+    cout << endl;
+
+    cout << "Массив Y: ";
+    for (int i = 0; i < n; i++) {
+        cout << *(Y + i) << " ";
+    }
+    cout << endl;
+
+    peres(X, Y, k, n);
+
+    delete[] X;
+    delete[] Y;
+
+    return 0;
 }
