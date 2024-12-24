7ee318c4c04bdf0fd3c0ce3b3b774e80696802f8
m0loko <gittigwork@gmail.com>
2024-12-24 20:16:04 +0300

    10th commit without good folder structuring

--- 1_semester/ОАИП(Основы Алгоритмизации и Программирования)/лабы/лаба_17/lab_17/lab_17/main.cpp
+++ 1_semester/ОАИП(Основы Алгоритмизации и Программирования)/лабы/лаба_17/lab_17/lab_17/main.cpp
@@ -1,28 +1,133 @@
 #include <iostream>
+#include <windows.h>
 using namespace std;
 
-double& dmin(double A[], int size);
+int* task1(int*, int);
+void task2(int**, int, int);
 
-void main()
-{
-	double s;
-	const int size = 5;
-	double A[] = { 5, 4.1, 3, 0.2, 11 };
-	s = dmin(A, size);
-	cout << s << endl;
-	for (int i = 0; i < size; i++)
-		cout << " " << A[i];
-	cout << endl;
-	dmin(A, size) = 1.0;       // изменение минимума на значение 1.0 
-	for (int i = 0; i < size; i++)
-		cout << " " << A[i];
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int choice;
+
+    do {
+        cout << "Какое задание нужно показать" << endl;
+        cout << "1 - первое задание" << endl;
+        cout << "2 - второе задание" << endl;
+        cout << "3 - выход" << endl;
+        cin >> choice;
+
+        switch (choice) {
+        case 1: {
+            int n;
+            cout << "Введите размер одномерного массива: ";
+            cin >> n;
+
+            int* A = new int[n];
+            cout << "Введите элементы массива:\n";
+            for (int i = 0; i < n; i++) {
+                cin >> A[i];
+            }
+
+            int* count = task1(A, n);
+            if (*count > 0) {
+                cout << "Количество отрицательных элементов на четных местах: " << *count << endl;
+            }
+            else {
+                cout << "Таких элементов нет\n";
+            }
+            delete count;
+            delete[] A;
+
+            system("pause");
+            system("cls");
+            break;
+        }
+        case 2: {
+            int n, m;
+            cout << "Введите количество строк и столбцов матрицы: ";
+            cin >> n >> m;
+
+            int** matr = new int* [n];
+            for (int i = 0; i < n; i++) {
+                matr[i] = new int[m];
+            }
+
+            cout << "Заполните матрицу:\n";
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    cin >> matr[i][j];
+                }
+            }
+
+            task2(matr, n, m);
+
+            for (int i = 0; i < n; i++) {
+                delete[] matr[i];
+            }
+            delete[] matr;
+
+            system("pause");
+            system("cls");
+            break;
+        }
+        case 3:
+            break;
+        }
+    } while (choice != 3);
+
+    return 0;
 }
 
-double& dmin(double A[], int size)
-{
-	int i, j = 0;
-	for (i = 1; i < size; i++)
-		if (A[j] > A[i])
-			j = i;
-	return A[j];
+int* task1(int* A, int n) {
+    int* count_o = new int(0);
+    for (int i = 0; i < n; i++) {
+        if (i % 2 == 0 && A[i] < 0) {
+            (*count_o)++;
+        }
+    }
+    return count_o;
+}
+
+void task2(int** matr, int n, int m) {
+    cout << "Исходная матрица:\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << matr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    bool found = false;
+    int rowIndex = -1;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (matr[i][j] < 0) {
+                rowIndex = i;
+                found = true;
+                break;
+            }
+        }
+        if (found) break;
+    }
+
+    if (found) {
+        cout << "Строка с отрицательным элементом: " << rowIndex + 1 << endl;
+        cout << "Обновленная матрица (элементы столбца " << rowIndex + 1 << " уменьшены вдвое):\n";
+
+        for (int i = 0; i < n; i++) {
+            matr[i][rowIndex] /= 2;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cout << matr[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+    else {
+        cout << "Нет строк с отрицательными элементами.\n";
+    }
 }
